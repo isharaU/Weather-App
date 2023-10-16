8d751f7a89665e031bc4cb327f9aae44ef20b84b
isharaU <isharauditha5@gmail.com>
2023-10-16 22:32:24 +0530

    load data from json

---
+++ Read_data.py
@@ -0,0 +1,13 @@
+import json
+
+# load data from json file
+with open ('cities.json') as json_file:
+    data = json.load(json_file)
+    
+# extract the city codes
+city_codes = []
+
+for city in data['List']:
+    city_codes.append(city['CityCode'])
+
+print(city_codes)
