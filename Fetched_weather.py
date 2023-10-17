1501ae718e52af97477e10d54d42600cd546abbd
isharaU <isharauditha5@gmail.com>
2023-10-17 05:50:39 +0530

    ignore weather_data.json

--- Fetched_weather.py
+++ Fetched_weather.py
@@ -8,16 +8,15 @@ api_key = config('WEATHER_API_KEY')
 # List of city IDs from Read_data.py
 city_ids = ['1248991', '1850147', '2644210', '2988507', '2147714', '4930956', '1796236', '3143244']  
 
-# API request
-url = f"http://api.openweathermap.org/data/2.5/group?id={','.join(map(str, city_ids))}&units=metric&appid={api_key}"
 
-# GET request to the OpenWeatherMap API
+url = f"http://api.openweathermap.org/data/2.5/group?id={','.join(map(str, city_ids))}&units=metric&appid={api_key}"
 response = requests.get(url)
 
 if response.status_code == 200:
+    # Convert the response to JSON format
     weather_data = response.json()
     
-    with open('weather.json', 'w') as json_file:
+    with open('weather_data.json', 'w') as json_file:
         json.dump(weather_data, json_file)
   
 else:
